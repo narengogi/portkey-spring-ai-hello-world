75cba5f343bed55a58b6a48d2b5182254cebee86
Narendranath Gogineni <47327611+narengogi@users.noreply.github.com>
2024-07-13 17:29:22 +0530

    Update ChatClient Bean to Portkey

--- src/main/java/org/springframework/ai/openai/samples/helloworld/Config.java
+++ src/main/java/org/springframework/ai/openai/samples/helloworld/Config.java
@@ -1,15 +1,33 @@
 package org.springframework.ai.openai.samples.helloworld;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.function.Consumer;
 
 @Configuration
 class Config {
 
+    String baseUrl = "https://api.portkey.ai";
+
+    Consumer<HttpHeaders> defaultHeaders = header -> {
+        header.add("x-portkey-api-key", "{{PORTKEY_API_KEY}}");
+        header.add("x-portkey-config", "{{PORTKEY_CONFIG}}");
+    };
+
     @Bean
-    ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.build();
+    ChatClient chatClient() {
+        RestClient.Builder restClientBuilder = RestClient.builder().defaultHeaders(defaultHeaders);
+        WebClient.Builder webClientBuilder = WebClient.builder().defaultHeaders(defaultHeaders);
+        OpenAiApi openAiApi = new OpenAiApi(baseUrl, "DUMMY_KEY", restClientBuilder, webClientBuilder);
+        OpenAiChatModel openAiChatModel = new OpenAiChatModel(openAiApi);
+        return ChatClient.create(openAiChatModel);
     }
 
 }
